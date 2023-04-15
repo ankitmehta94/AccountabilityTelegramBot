35ca15930c26ee220b0fdbc6767d25346c7185c7
Ankit Mehta <ankitmehta94@gmail.com>
2023-04-15 12:16:43 -0400

    added langchain and tested it works

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // Import necessary libraries
 import { Telegraf } from "telegraf";
-import generateChatCompletion from "./openai.js";
-import config from "./env.json";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanChatMessage } from "langchain/schema";
+import config from "./env.json" assert { type: "json" };
 
 // Create an instance of the Telegram bot
 const bot = new Telegraf(config.TELEGRAM_TOKEN);
-
+const chat = new ChatOpenAI({
+  temperature: 0,
+  openAIApiKey: config.OPEN_API_SECTRET_KEY,
+});
 // Start listening for messages
 bot.start((ctx) => {
-  console.log("Heyu");
   ctx.reply(
     "Welcome to the ChatGPT bot! Send me a message and I will generate a response using GPT-3."
   );
@@ -19,10 +22,10 @@ bot.on("text", async (ctx) => {
   const userInput = ctx.message.text;
 
   // Call GPT-3 to generate a response
-  const response = await generateChatCompletion(userInput);
+  const response = await chat.call([new HumanChatMessage(userInput)]);
 
   // Send the generated response back to the user
-  ctx.reply(response);
+  ctx.reply(response.text);
 });
 
 // Start the bot
